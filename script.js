bc0338a9253cd28ca77e3bff825f0786088e736e
BrokenRolz <miyukihuang666@gmail.com>
2023-02-02 11:52:54 +0530

    Add function playRound
    Added a function to play a round of the game and also made the function
    parameter case -insensitive.

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-const choice = ["Rock", "Paper", "Scissors"];
+const choice = ["Rock", "Paper", "Scissor"];
 
 function getComputerChoice(choice) {
    return choice[Math.floor(Math.random()*choice.length)];
 }
 // console.log(getComputerChoice(choice));
+
+function playRound(playerSelection, computerSelection) {
+   computerSelection = getComputerChoice(choice).toLowerCase();
+   playerSelection = playerSelection.toLowerCase();
+   if (playerSelection == computerSelection){
+      return "Tie";
+   }else if(
+      (playerSelection == "rock" && computerSelection == "paper") ||
+      (playerSelection == "paper" && computerSelection == "scissor") ||
+      (playerSelection == "scissor" && computerSelection == "rock")
+   )  {return "You Lost";
+   }else if(
+      (computerSelection == "rock" && playerSelection == "paper") ||
+      (computerSelection == "paper" && playerSelection == "scissor") ||
+      (computerSelection == "scissor" && playerSelection == "rock")
+   ){return "You Won";}
+}
+
+// const playerSelection = "Scissor";
+// const computerSelection = getComputerChoice(choice);
+// console.log(playRound(playerSelection, computerSelection));
